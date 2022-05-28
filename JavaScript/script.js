fe1c984ce8a6731a613e06e9a812356280e78f9e
xman6565 <56615038+xman6565@users.noreply.github.com>
2022-05-28 22:14:15 +0900

    javaScript 연습 1

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,15 @@
-alert("hi");
+/* const : 변수 재선언,재할당 모두 불가능*/
+const a = 10;
+const b = 2;
+
+/* let : 번수 재할당이 가능 */
+let c = 3;
+
+
+
+console.log(a+b+c); //15
+console.log(a*b*c); //60
+console.log(a/b/c); // 1.66666666666667
+
+c=2
+console.log(a+b+c); //14
