cfec8e8cf459ba2e683d6f7b46c6136611fe0841
xman6565 <56615038+xman6565@users.noreply.github.com>
2022-05-29 21:13:03 +0900

    JavaScript 연습 2일차

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,3 +13,32 @@ console.log(a/b/c); // 1.66666666666667
 
 c=2
 console.log(a+b+c); //14
+
+
+
+/* boolean */
+/* boolean의 값은 true,false,null, undefined */
+const amIFat = null;
+let something;
+console.log(something, amIFat);
+
+/* Array 배열 */
+const mon = "mon";
+const tue = "tue";
+const wen = "wen";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const days0fweek = ["mon" , "tue" , "wen" , "thu" , "fri" , "sta"];
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "hihi"];
+
+// array에서 값 받아오기
+console.log(days0fweek);
+console.log(nonsense[2]);
+
+// array 안에 요일 더 받아오기
+days0fweek.push("sun");
+console.log(days0fweek);
